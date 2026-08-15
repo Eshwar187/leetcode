3e39de0475051557b2c3e98a7ea6d63794e9164b
Eshwar J <132641767+Eshwar187@users.noreply.github.com>
2026-08-15 19:31:17 +0530

    Time: 3 ms (57.64%), Space: 44.2 MB (83.78%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            Character left=s.charAt(i);
+            Character right=s.charAt(j);
+            if(!Character.isLetterOrDigit(left)){
+                i++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(right)){
+                j--;
+                continue;
+            }
+            if(Character.toLowerCase(left)!=Character.toLowerCase(right)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
